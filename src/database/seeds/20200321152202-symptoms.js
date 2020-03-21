cade79da7a852d5a7245c51defe7de051ffcdd56
Manogel <manoelgomes53@gmail.com>
2020-03-21 12:28:01 -0300

    seed symptoms

---
+++ src/database/seeds/20200321152202-symptoms.js
@@ -0,0 +1,61 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.bulkInsert(
+      'symptoms',
+      [
+        {
+          name: 'Congestão nasal (Nariz entupido)',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Corrimento nasal (Nariz escorrendo)',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Dificuldade para respirar',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Febre (Acima de 37.5°)',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Dores pelo corpo',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Mal-estar geral',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Dor de garganta',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Dor de cabeça',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Tosse',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Cansaço',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    ),
+
+  down: queryInterface => queryInterface.bulkDelete('symptoms', null, {}),
+};
